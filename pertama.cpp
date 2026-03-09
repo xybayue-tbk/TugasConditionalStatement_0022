c0ee51b51bf2ca6efa0b6d0687995c852603bd06
xybayue-tbk <xybayue@gmail.com>
2026-03-09 21:00:36 +0700

    Menambahkan fungsi

--- pertama.cpp
+++ pertama.cpp
@@ -4,5 +4,11 @@ using namespace std;
 float Berat, Tinggi;
 
 void inputData(){
-    
+    cout << std::endl;
+    cout << "--- Selamat Datang Mahasiswa Kelas A 2025 ---" << endl;
+    cout << "Masukkan Berat Badan (kg): ";
+    cin >> Berat;
+    cout << "Masukkan Tinggi Badan (m): ";
+    cin >> Tinggi;
+
 }
